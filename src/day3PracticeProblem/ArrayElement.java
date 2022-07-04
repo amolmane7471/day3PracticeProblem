c3b89e8fb31dbe4dc436dd879fd8c18a2f98f2f0
amolmane7471 <maneamol3233@gmail.com>
2022-07-04 15:45:30 +0530

    Added

---
+++ src/day3PracticeProblem/ArrayElement.java
@@ -0,0 +1,18 @@
+package day3PracticeProblem;
+public class ArrayElement {
+
+	public static void main(String[] args) {
+         int[] array= {10,20,30,40,50};
+         System.out.println("Array Elements Are:");
+         for (int i=0;i<array.length;i++)
+         {
+        	 
+         System.out.println(+array[i]);
+                 }
+	System.out.println();
+    for (int i=0;i<array.length;i++)
+    {
+    System.out.print(+array[i]+"  ");
+    }
+    }
+}
